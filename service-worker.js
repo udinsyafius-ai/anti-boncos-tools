228ffb3c1d8eee6740ba3d54760598c6e2872d23
udinsyafius-ai <udin.syafi.us@gmail.com>
2026-03-14 09:52:25 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "anti-boncos-cache-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/app.html",
+"/manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(function(cache) {
+return cache.addAll(urlsToCache);
+})
+);
+});
+
+self.addEventListener("fetch", function(event) {
+event.respondWith(
+caches.match(event.request)
+.then(function(response) {
+return response || fetch(event.request);
+})
+);
+});
